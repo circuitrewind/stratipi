4c865c27fcacc5b2818f86e862aeda3a3e248e29
Vince <git@darkain.com>
2026-02-10 03:40:55 +0000

    Fix? #6 - Use temp path and temp pool name when building image

--- build.sh
+++ build.sh
@@ -3,16 +3,21 @@ set -eu
 
 
 ZPOOL=stratipi
-LABEL=STRATIPI
+
 IMAGE_SIZE=2G
+
 ARCH=aarch64
 ABI=FreeBSD:15:$ARCH
 OSVERSION=1500000
+
+LABEL=$(echo "$ZPOOL" | tr '[:lower:]' '[:upper:]')
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 IMAGE=$SCRIPT_DIR/${ZPOOL}.img
 PARTITION=mbr
 DEVICE=""
+ROOT=""
+POOL=""
 
 
 # PRETTY PRINT A STATUS LINE
@@ -49,8 +54,8 @@ safe_export() {
 # DO ALL THE CLEANUP STUFF
 cleanup() {
 	println "Running cleanup job ..."
-	safe_umount "/$ZPOOL/boot/efi" || true
-	safe_export $ZPOOL || true
+	[ -n "$ROOT" ] && safe_umount "$ROOT/boot/efi" || true
+	[ -n "$POOL" ] && safe_export $POOL || true
 	mdconfig -d -u $DEVICE 2>/dev/null || true
 }
 
@@ -66,6 +71,11 @@ esac
 
 # INSTALL OUR TRAPS LATE, IN CASE OF CUSTOM COMMAND ABOVE
 trap cleanup EXIT INT TERM
+
+
+# CREATE OUR TEMPORARY DIRECTORY
+ROOT=$(mktemp -d)
+POOL=$(basename $ROOT)
 
 
 # THINGS WE'LL NEED LATER ON IN THE SCRIPT
@@ -100,13 +110,18 @@ else
 	println "Unknown Partition Table Type"
 	exit 1
 fi
-
+echo ""
+gpart show $DEVICE
 
 
 
 # CREATE AND MOUNT THE ZPOOL/ZFS FILE SYSTEM
 # MUST COME BEFORE FAT32 DUE TO MOUNT POINTS
-println "Creating zpool: $ZPOOL on ${DEVICE}${SLICE}2"
+# SMALL O: ZPOOL PROPERTIES (PAY ATTENTION!)
+# BIG O: ZFS DATASET PROPERTIES
+println "Creating zpool: $ZPOOL ($POOL) on ${DEVICE}${SLICE}2"
+ZROOT=$(dirname $ROOT)
+set -x
 zpool create -f \
   -o ashift=12 \
   -o autotrim=off \
@@ -114,8 +129,12 @@ zpool create -f \
   -O recordsize=16M \
   -O compression=zstd-9 \
   -O sync=disabled \
+  -t $POOL \
+  -R $ZROOT \
   $ZPOOL "${DEVICE}${SLICE}2"
-zpool list $ZPOOL
+{ set +x; } 2>/dev/null
+zpool list $POOL
+
 
 
 # CREATE AND MOUNT THE MSDOS FAT32 FILE SYSTEM
@@ -123,14 +142,14 @@ println "Creating FAT32 file system on ${DEVICE}${SLICE}1"
 newfs_msdos -F 32 -S 512 -c 1 -L "EFIBOOT" "${DEVICE}${SLICE}1"
 
 
-mkdir -p /$ZPOOL/boot/efi
-mount -t msdosfs "${DEVICE}${SLICE}1" "/$ZPOOL/boot/efi"
+mkdir -p $ROOT/boot/efi
+mount -t msdosfs "${DEVICE}${SLICE}1" "$ROOT/boot/efi"
 
 
 
 # COPY RASPBERRY PI FIRMWARE TO OUR EFIBOOT PARTITION
 println "Copying firmware to EFI partition"
-cp -r /usr/local/share/rpi-firmware/* /$ZPOOL/boot/efi/
+cp -vr /usr/local/share/rpi-firmware/* $ROOT/boot/efi/
 
 
 
@@ -138,33 +157,33 @@ cp -r /usr/local/share/rpi-firmware/* /$ZPOOL/boot/efi/
 # THIS ALSO SPEEDS UP REBUILDING THE IMAGE FOR DEVELOPMENT
 println "Setting up local package cache on host machine"
 mkdir -p /var/cache/$ZPOOL/$ARCH/repos/
-mkdir -p /$ZPOOL/var/cache/
-#mkdir -p /$ZPOOL/var/db/pkg/
-ln -s /var/cache/$ZPOOL/$ARCH/ /$ZPOOL/var/cache/pkg
-#ln -s /var/cache/$ZPOOL/$ARCH/repos/ /$ZPOOL/var/db/pkg/repos
+mkdir -p $ROOT/var/cache/
+#mkdir -p $ROOT/var/db/pkg/
+ln -s /var/cache/$ZPOOL/$ARCH/ $ROOT/var/cache/pkg
+#ln -s /var/cache/$ZPOOL/$ARCH/repos/ $ROOT/var/db/pkg/repos
 
 
 
 # PREPARE FREEBSD PKG KEYS
 println "Setting up package repositories"
-mkdir -p /$ZPOOL/usr/share/keys/pkg/trusted
-cp /usr/share/keys/pkg/trusted/* /$ZPOOL/usr/share/keys/pkg/trusted/
+mkdir -p $ROOT/usr/share/keys/pkg/trusted
+cp /usr/share/keys/pkg/trusted/* $ROOT/usr/share/keys/pkg/trusted/
 
 # PREPARE FREEBSD PKG-BASE KEYS
-mkdir -p /$ZPOOL/usr/share/keys/pkgbase-15/trusted
-cp /usr/share/keys/pkgbase-15/trusted/* /$ZPOOL/usr/share/keys/pkgbase-15/trusted/
+mkdir -p $ROOT/usr/share/keys/pkgbase-15/trusted
+cp /usr/share/keys/pkgbase-15/trusted/* $ROOT/usr/share/keys/pkgbase-15/trusted/
 
 # PREPARE FREEBSD PKG CONFIGURATION
-mkdir -p /$ZPOOL/etc/pkg
-cp /etc/pkg/FreeBSD.conf /$ZPOOL/etc/pkg
-sed -i '' 's/: no/: yes/' /$ZPOOL/etc/pkg/FreeBSD.conf
-sed -i '' 's/quarterly"/latest"/' /$ZPOOL/etc/pkg/FreeBSD.conf
+mkdir -p $ROOT/etc/pkg
+cp /etc/pkg/FreeBSD.conf $ROOT/etc/pkg
+sed -i '' 's/: no/: yes/' $ROOT/etc/pkg/FreeBSD.conf
+sed -i '' 's/quarterly"/latest"/' $ROOT/etc/pkg/FreeBSD.conf
 
 
 
 # WARNING, DON'T MOVE THIS EARLIER IN THE SCRIPT
 # OR ELSE YOU RISK BREAKING YOUR ENTIRE OPERATING SYSTEM
-METALOG=/$ZPOOL/$ZPOOL.metalog
+METALOG=$ROOT/$ZPOOL.metalog
 export METALOG
 export ABI
 export OSVERSION
@@ -174,16 +193,16 @@ export OSVERSION
 println "Installing FreeBSD pkgbase and user packages"
 PACKAGES=$(sed 's/#.*//' $SCRIPT_DIR/pkglist)
 [ -n "$PACKAGES" ] || { println "No packages to install!"; exit 1; }
-pkg -r /$ZPOOL -o REPOS_DIR=/$ZPOOL/etc/pkg install -y $PACKAGES
+pkg -r $ROOT -o REPOS_DIR=$ROOT/etc/pkg install -y $PACKAGES
 
 
 # STORE PACKAGES/VERSIONS USED FOR THE BUILD IN AN AUDIT LOG
-pkg -r /$ZPOOL query '%n-%v' > $SCRIPT_DIR/pkg-manifest.txt
+pkg -r $ROOT query '%n-%v' > $SCRIPT_DIR/pkg-manifest.txt
 
 
 # FIX FILE/FOLDER PERMISSIONS FOR CUSTOM USERS
 println "Fixing file and folder permissions"
-$SCRIPT_DIR/uid.sh $METALOG /$ZPOOL
+$SCRIPT_DIR/uid.sh $METALOG $ROOT
 rm $METALOG
 
 
@@ -192,36 +211,39 @@ println "Installing $ZPOOL files"
 touch $SCRIPT_DIR/var/db/last_time
 for f in $SCRIPT_DIR/*; do
     [ -f $(basename -- "$f") ] && continue
-    cp -r "$f" /$ZPOOL/
+    cp -vr "$f" $ROOT
 done
 
 
 
 # INSTALL THE BOOTLOADER
 println "Installing the FreeBSD boot loader"
-mkdir -p /$ZPOOL/boot/efi/EFI/BOOT/
-cp /$ZPOOL/boot/loader.efi /$ZPOOL/boot/efi/EFI/BOOT/bootaa64.efi
+mkdir -p $ROOT/boot/efi/EFI/BOOT/
+cp -v $ROOT/boot/loader.efi $ROOT/boot/efi/EFI/BOOT/bootaa64.efi
 
 
 # CREATE ZPOOL SCRUB/TRIM CRONJOB
 println "Creating zpool scrub and trim cron jobs"
-mkdir -p /$ZPOOL/etc/cron.d/
-echo "@daily	root	/sbin/zpool scrub $ZPOOL" > /$ZPOOL/etc/cron.d/$ZPOOL
-echo "@weekly	root	/sbin/zpool trim $ZPOOL" >> /$ZPOOL/etc/cron.d/$ZPOOL
+mkdir -p $ROOT/etc/cron.d/
+echo "@daily	root	/sbin/zpool scrub $ZPOOL" > $ROOT/etc/cron.d/$ZPOOL
+echo "@weekly	root	/sbin/zpool trim $ZPOOL" >> $ROOT/etc/cron.d/$ZPOOL
+cat $ROOT/etc/cron.d/$ZPOOL
 
 
 # CLEANUP TEMPORARY CACHE SYMLINK
 println "Unlinking package cache"
-rm /$ZPOOL/var/cache/pkg
+rm $ROOT/var/cache/pkg
 
 
 # SET ZFS PROPERTIES TO SOMETHING SANE FOR NORMAL USAGE
 println "Setting 'sane' zpool options for daily usage"
+set -x
 zfs set \
   compression=on \
   recordsize=128k \
   sync=standard \
-  $ZPOOL
+  $POOL
+{ set +x; } 2>/dev/null
 
 
 # CLEANUP ALL THE TEMPORARY STUFF WE DID
